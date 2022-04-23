f7c0b2a5ff39a3fda7bf2f5c51987fcaf0559634
Edwin Roman <romanseberino@gmail.com>
2022-04-23 17:41:21 -0400

    Improving the way of catch errors.
    Improving the way of catch errors, here I'm stooping the scripts after any error and showing it up in a console. Some recommendation to include in a future work.

--- controller/errorHandler.js
+++ controller/errorHandler.js
@@ -1,11 +1,16 @@
 // Functions for the MQTT errors.
 
 const mqttErrors = (err) => {
- 
-    console.log(err);
-    this.mqttClient.end(true);
 
-}
+    // TODO: Set a reconnect time to avoid the immediately close or to determinate if 
+    // TODO: the library or module has something to manage the timeOut of reconnection.
+
+    console.log(err.message);
+    this.mqttClient.end();
+    clearInterval(this.objSetInterval);
+    process.exit(0);
+
+};
 
 module.exports = {
     mqttErrors
